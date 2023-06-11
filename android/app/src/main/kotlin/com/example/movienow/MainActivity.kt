62f5cdaa7ec2f523dc1c4d1822ff096dc52b9779
JArmandoTGProMX <armandopromx@gmail.com>
2023-06-10 17:29:37 -0700

    First Commit

---
+++ android/app/src/main/kotlin/com/example/movienow/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.movienow
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/domain/datasources/movies_datasource.dart
@@ -0,0 +1,5 @@
+import 'package:movienow/domain/entities/movie.dart';
+
+abstract class MovieDataSource {
+  Future<List<Movie>> getNowPlaying({int page = 1});
+}
